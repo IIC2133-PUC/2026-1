95ffcf6e87fa675b4d2214f2a68ebad9d767383a
orbisai0security <mediratta01.pally@gmail.com>
2026-06-28 03:37:21 +0000

    fix: c.lang.security.insecure-use-scanf-fn.insecure-use-scanf-fn security vulnerability
    Automated security fix generated by OrbisAI Security

--- Ayudantías/Material extra/Ejercicios de C/3 problemas introductorios/Ejercicio 1/ej1.c
+++ Ayudantías/Material extra/Ejercicios de C/3 problemas introductorios/Ejercicio 1/ej1.c
@@ -7,7 +7,9 @@ int main() {
 
     /* Este código tiene 4 errores */
     n;
-    scanf("%d", &n);
+    char buf[32];
+    fgets(buf, sizeof(buf), stdin);
+    sscanf(buf, "%d", &n);
     for (int i; i < n; i++){
         for (int j; j < i + 1; j++){
             printf("*");
